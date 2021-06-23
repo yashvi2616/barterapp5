188b45191d198e245ff1d5bb14e841f67632655f
Yashvi <ermanojpatel7005@gamil.com>
2021-06-23 18:28:34 +0530

    first commit

---
+++ components/MyHeader.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyeSheet ,Alert} from 'react-native';
+
+const MyHeader = props => {
+  return (
+    <Header
+      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
+      centerComponent={{ text: props.title, style: {color: '#000', fontSize:20,fontWeight:"bold", } }}
+      backgroundColor = "#FFFF01"
+    />
+  );
+};
+
+export default MyHeader;
